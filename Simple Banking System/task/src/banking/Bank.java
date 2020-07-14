d6314e50f758c2b8844d9a7fcc738d14064a54b1
alicenychto <irol2273@icloud.com>
2020-07-14 14:08:00 +0200

    stage 1 cleared

---
+++ Simple Banking System/task/src/banking/Bank.java
@@ -0,0 +1,127 @@
+package banking;
+
+import java.math.BigInteger;
+import java.util.Map;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.TreeMap;
+
+enum State {
+    MENU1, CREATE, LOGIN, MENU2, BALANCE, LOGOUT, EXIT
+}
+
+public class Bank {
+    Scanner sc = new Scanner(System.in);
+    Random r = new Random();
+    State state;
+
+    private final Map<String, String>  users;
+    private final Map<String, Integer> balances;
+    private String activeUser;
+
+    public Bank() {
+        state = State.MENU1;
+        users = new TreeMap<>();
+        balances = new TreeMap<>();
+        activeUser = "";
+    }
+
+    public void process() {
+        switch (state) {
+            case MENU1: {
+                System.out.println("1. Create an account");
+                System.out.println("2. Log into account");
+                System.out.println("0. Exit");
+                int ans = Integer.parseInt(sc.nextLine());
+                switch (ans) {
+                    case 1: {
+                        state = State.CREATE;
+                        break;
+                    }
+                    case 2: {
+                        state = State.LOGIN;
+                        break;
+                    }
+                    case 0: {
+                        System.out.println("Bye!");
+                        state = State.EXIT;
+                        break;
+                    }
+                }
+                break;
+            }
+            case CREATE: {
+                String card;
+                do {
+                    card = "400000";
+                    for (int i = 0; i < 9; i++)
+                        card += r.nextInt(10);
+                    card += "5";
+                } while (users.containsKey(card));
+                String pin = "";
+                for (int i = 0; i < 4; i++)
+                    pin += r.nextInt(10);
+                users.put(card, pin);
+                balances.put(card, 0);
+                System.out.println("Your card has been created");
+                System.out.println("Your card number:");
+                System.out.println(card);
+                System.out.println("Your card PIN:");
+                System.out.println(pin);
+                state = State.MENU1;
+                break;
+            }
+            case LOGIN: {
+                System.out.println("Enter your card number:");
+                String card = sc.nextLine();
+                System.out.println("Enter your PIN:");
+                String pin = sc.nextLine();
+                if (users.get(card) != null && users.get(card).equals(pin)) {
+                    activeUser = card;
+                    System.out.println("You have successfully logged in!");
+                    state = State.MENU2;
+                } else {
+                    System.out.println("Wrong card number or PIN!");
+                    state = State.MENU1;
+                }
+                break;
+            }
+            case MENU2: {
+                System.out.println("1. Balance");
+                System.out.println("2. Log out");
+                System.out.println("0. Exit");
+                int ans = Integer.parseInt(sc.nextLine());
+                switch (ans) {
+                    case 1: {
+                        state = State.BALANCE;
+                        break;
+                    }
+                    case 2: {
+                        state = State.LOGOUT;
+                        break;
+                    }
+                    case 0: {
+                        System.out.println("Bye!");
+                        state = State.EXIT;
+                        break;
+                    }
+                }
+                break;
+            }
+            case BALANCE: {
+                System.out.println(balances.get(activeUser));
+                state = State.MENU2;
+                break;
+            }
+            case LOGOUT: {
+                System.out.println("You have successfully logged out!");
+                activeUser = "";
+                state = State.MENU1;
+                break;
+            }
+            case EXIT: {
+                break;
+            }
+        }
+    }
+}
---
+++ Simple Banking System/task/src/banking/Main.java
@@ -0,0 +1,13 @@
+package banking;
+
+
+import static banking.State.EXIT;
+
+public class Main {
+    public static void main(String[] args) {
+        Bank bank = new Bank();
+        while (bank.state != EXIT) {
+            bank.process();
+        }
+    }
+}
